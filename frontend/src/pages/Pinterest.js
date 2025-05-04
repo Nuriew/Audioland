7fb71d7bb869612f21ed8852bf9edfcd5e426c3f
Nuriew <93245587+Nuriew@users.noreply.github.com>
2025-05-04 19:58:56 +0400

    Update Pinterest.js

--- frontend/src/pages/Pinterest.js
+++ frontend/src/pages/Pinterest.js
@@ -19,7 +19,7 @@ function Pinterest() {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/pinterest', {
+      const response = await fetch('http://173.249.60.195:5000/api/pinterest', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ url: link }),
