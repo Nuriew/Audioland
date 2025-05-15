c5ee3e00e19b3a7a61b0db1b2466d99402a58744
Nuriew <93245587+Nuriew@users.noreply.github.com>
2025-05-15 19:17:03 +0400

    Update Youtube.js

--- frontend/src/pages/Youtube.js
+++ frontend/src/pages/Youtube.js
@@ -19,10 +19,10 @@ function Youtube() {
     }
 
     try {
-      const response = await fetch('http://173.249.60.195:5000/api/youtube', {
+      const response = await fetch('https://173.249.60.195:5000/api/youtube', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ video_url: link }), // <-- burası düzeltildi
+        body: JSON.stringify({ video_url: link }), 
       });
 
       const data = await response.json();
